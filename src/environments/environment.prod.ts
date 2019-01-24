b081ade21d8a51b90d95178a01e195740c620806
Max Carlquist <tenkaklet@gmail.com>
2019-01-24 08:49:11 +0100

    made prod true

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA4B0irdBRBwBHxYzCBNyoBGAHWfyRnoaU",
+    authDomain: "github-bb0b6.firebaseapp.com",
+    databaseURL: "https://github-bb0b6.firebaseio.com",
+    projectId: "github-bb0b6",
+    storageBucket: "github-bb0b6.appspot.com",
+    messagingSenderId: "797750465656"
+  }
 };
